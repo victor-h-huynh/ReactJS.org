037cb6b3851de496cea60baddb458ba5bc2c121e
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-16 20:08:41 +0000

    consoleLog variations

--- exercises/app.js
+++ exercises/app.js
@@ -1,1 +1,21 @@
-alert('Hello World');
+// Log to console
+console.log("Hello World");
+console.log(123);
+console.log(true);
+
+var greeting = "Hello";
+console.log(greeting);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error("ERROR ERROR");
+console.clear();
+console.warn("This is a warning");
+
+console.time('Hello');
+  console.log('Hello World')
+  console.log('Hello World')
+  console.log('Hello World')
+  console.log('Hello World')
+console.timeEnd("Hello");
