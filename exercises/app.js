faa7e311276d1b580c9c5c73f4599bfbe471a1af
Victor Huynh <victor.h.huynh@gmail.com>
2019-08-17 19:33:00 +0000

    if and or ternary logical operator

--- exercises/app.js
+++ exercises/app.js
@@ -504,3 +504,98 @@
 // birthday.setSeconds(25);
 
 // console.log(birthday);
+
+// ================================================================================== //
+
+// IF STATEMENTS August 17 2019
+
+// ================================================================================== //
+
+const id = 100;
+
+// Equal to
+
+// if (id == 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // Not Equal to
+
+// if(id != 101) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // Equal to value & type
+// if (id === 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// // Equal to value & type
+// if (id !== 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+
+// Test if undefined
+// if (typeof id !== 'undefined') {
+//   console.log(`The ID is ${id}`);
+// } else {
+//   console.log('NO ID');
+// }
+
+// Greater or Less than
+// if (id <= 100) {
+//   console.log('CORRECT');
+// } else {
+//   console.log('INCORRECT');
+// }
+
+// If else 
+
+const color = 'yellow';
+
+// if(color === 'red') {
+//   console.log('Color is red');
+// } else if (color === 'blue') {
+//   console.log('Color is blue');
+// } else {
+//   console.log(`Color is ${color}`)
+// }
+
+// Logical Operators
+
+const name = 'Steve';
+const age = 65;
+
+// And &&
+if (age > 0 && age < 12) {
+  console.log(`${name} is a child`);
+} else if (age >= 13 && age <= 19){
+  console.log(`${name} is a teengaer`);
+} else {
+  console.log(`${name} is an adult`);
+}
+
+// Or || 
+if(age < 16 || age > 65) {
+  console.log(`${name} can not run in race`);
+} else {
+  console.log(`${name} is regisered for the race`);
+}
+
+// Ternary Operator
+console.log(id === 100 ? 'CORRECT' : 'INCORRECT');
+
+// Without braces
+if (id === 100)
+console.log('CORRECT');
+else
+console.log('INCORRECT');
